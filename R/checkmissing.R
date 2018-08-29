3f4c2025777037e65f942e2046772f057c58aed6
root <root@linux.home>
2018-08-29 13:44:09 +0200

    Ajout de checkMissing

--- R/checkmissing.R
+++ R/checkmissing.R
@@ -1,101 +1,53 @@
-library(methods)
+checkMissing <- function(data, vars=NULL, sort) {
+  Names = names(data)
+  if (is.null(vars) == FALSE) {
+    Names = vars;
+  }
 
-setClass("ec.Missing.check",
-         # ==== Inheritance
-         # ==== Properties
-         representation (
-           varname   = "character",
-           detail    = "logical",
-           missdesc  = "data.frame",
-           missbyrow = "data.frame"
-         )           
-)
+  # Simple description of missing values
+  # ------------------------------------
+  missdesc <- function(Names) {
+    Colnames = c("Variable", "Missing", "% Missing");
+    Effectif = nrow(data);
+    Missing  = c();
+    PMissing = c();
 
-# ------------------------------------------------------------------------------
-# Real constructor
-# ------------------------------------------------------------------------------
-setMethod("initialize", "ec.Missing.check",
-          function(.Object, vars, sort)
-          {
-            Names = names(GDS)
-            if (length(vars) > 0) {
-              Names = vars;
-            }
-            
-            # Simple description of missing values
-            # ------------------------------------
-            missdesc <- function(Names)
-            {
-              Colnames = c("Variable", "Missing", "% Missing");
-              Effectif = nrow(GDS);
-              Missing  = vector();
-              PMissing = vector();
-              
-              for (N in Names) {
-                Miss = sum(is.na(VAL(N)));
-                PMiss = sprintf("%5.2f", (Miss / Effectif) * 100);
-                Missing = c(Missing, Miss);
-                PMissing = c(PMissing, PMiss);
-              }
-  
-              DF = data.frame(cbind(Names, as.numeric(Missing), PMissing));
-              names(DF) <- Colnames;
-              DF[,2] <- as.numeric(as.character(DF[,2]));
-              DF[,3] <- as.numeric(as.character(DF[,3]));
-              if (sort == TRUE) {
-                DF <- DF[rev(order(DF[, "Missing"])),]
-              }
-              return(DF);
-            }
-
-            
-            missbyrow <- function(Names, Effectif)
-            {
-              Colnames = c("Miss/row", "Frequency", "Percent", "Cumul");
-              counts = apply(GDS,1,function(x) sum(is.na(x[Names])));
-              d <- as.data.frame(counts);
-              t <- table(counts);
-              vals = names(t);
-              t <- cbind(t, round(prop.table(t)*100,2));
-              DF1 <- as.data.frame(t);
-              Cum <- cumsum(DF1[,2]);
-              DF2 <- data.frame(cbind(vals, DF1$t, DF1$V2, Cum));
-              names(DF2) <- Colnames;
-              return(DF2);
-            }
-            
-            .Object@missdesc <- missdesc(Names);
-            .Object@missbyrow <- missbyrow(Names, Effectif);
-            
-            .Object;
-          });
-
-# -----------------------------------------------------------------------------
-# function: ec.Missing.check (call real constructor)
-# Return: an object of type ec.Missing.check
-# -----------------------------------------------------------------------------
-ec.Missing.check <- function(vars=vector(), sort=sort)
-{
-  return(new("ec.Missing.check", vars=vars, sort=sort));
-}
-
-# -----------------------------------------------------------------------------
-# method show
-# -----------------------------------------------------------------------------
-setMethod("show" ,"ec.Missing.check" ,
-    function(object){
-      cat("\n-----------------------------------\n");
-      print(object@missdesc);
-      cat("-----------------------------------\n");
-      cat("\n-----------------------------------\n");
-      print(object@missbyrow);
-      cat("-----------------------------------\n");
+    for (N in Names) {
+      Miss = sum(is.na(data[,N]));
+      PMiss = sprintf("%5.2f", (Miss / Effectif) * 100);
+      Missing = c(Missing, Miss);
+      PMissing = c(PMissing, PMiss);
     }
-)
 
-# library(Epiconcepts);
-# data(Tiramitsu)
-# ec.Use(Tiramitsu);
-# #P <- ec.Missing.check(vars=c("tira","wmousse","dmousse","beer"), sort=TRUE)
-# P <- ec.Missing.check(sort=TRUE)
-# print(P)
+    DF = data.frame(cbind(Names, as.numeric(Missing), PMissing))
+    names(DF) <- Colnames
+    DF[,2] <- as.numeric(as.character(DF[,2]))
+    DF[,3] <- as.numeric(as.character(DF[,3]))
+    if (sort == TRUE) {
+      DF <- DF[rev(order(DF[, "Missing"])),]
+      row.names(DF) <- c(1:nrow(DF))
+    }
+    return(DF);
+  }
+
+
+  missbyrow <- function(Names, Effectif) {
+    Colnames = c("Nb missing values", "Frequency", "Percent", "Cumul");
+    counts = apply(data, 1, function(x) sum(is.na(x[Names])));
+    d <- as.data.frame(counts);
+    t <- table(counts);
+    vals = names(t);
+    t <- cbind(t, round(prop.table(t)*100,2));
+    DF1 <- as.data.frame(t);
+    Cum <- cumsum(DF1[,2]);
+    DF2 <- data.frame(cbind(vals, DF1$t, DF1$V2, Cum));
+    names(DF2) <- Colnames;
+    return(DF2);
+  }
+
+  .df1 <- missdesc(Names)
+  .df2 <- missbyrow(Names, Effectif)
+  .L <- list(df1=.df1, df2=.df2)
+  .L
+
+}
