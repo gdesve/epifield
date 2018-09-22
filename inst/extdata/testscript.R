8492065aa95f1cefe59fc1e12762991b747bcce0
Gilles Desve <gdesve@gmail.com>
2018-09-22 23:25:48 +0200

    first attempt to recode

--- inst/extdata/testscript.R
+++ inst/extdata/testscript.R
@@ -1,7 +1,7 @@
 # pour tester certaines functions depuis un script
 #sink("inst/extdata/log.txt",append=FALSE)
-gastro5<-use("inst/extdata/gastro5.dta")
 #sink()
-
+vtest <- c(TRUE,TRUE,FALSE,FALSE)
+countif(vtest)
 
 
